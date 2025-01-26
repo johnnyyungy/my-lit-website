93238c1af7c3e8832ee27e8b5b7daa3f0df9d135
John Yung <johnnyyungy@gmail.com>
2025-01-26 10:33:48 +0000

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-import { LitElement, html } from 'lit';
-
-class TestComponent extends LitElement {
-  render() {
-    return html`<p>Hello, World!</p>`;
-  }
-}
-
-customElements.define('test-component', TestComponent);
