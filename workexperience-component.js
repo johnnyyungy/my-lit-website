03817c0d5bf3c848170c0dbd262499367b76ff67
John Yung <johnnyyungy@gmail.com>
2025-01-26 19:03:21 +0000

    fix

--- workexperience-component.js
+++ workexperience-component.js
@@ -18,6 +18,7 @@ export class WorkExperienceComponent extends LitElement {
   render() {
     return html`
       <link rel="stylesheet" href="./workexperience-styles.css">
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
       <section class="workexperience" id="work">
         <div class="wrapper">
           <h2>Work Experience</h2>
