a4b4f07f39fb600316f4fcbf05773616aaa97f14
John Yung <johnnyyungy@gmail.com>
2025-01-26 15:32:53 +0000

    fix

--- intro-component.js
+++ intro-component.js
@@ -4,57 +4,53 @@ import { globalStyles } from './global-styles.js';
 export class IntroComponent extends LitElement {
   static styles = [globalStyles, css`
 
-.intro {
-	background-color: #fff;
-	min-height: 400px;
-	display: flex;
-	
-}
+    /* Intro */
+    .intro {
+      background-color: #fff;
+      min-height: 400px;
+      display: flex;
+    }
 
-.intro .wrapper{
-	flex-direction: column;
-}
+    .intro .wrapper {
+      flex-direction: column;
+    }
 
-.intro .wrapper aside {
-	flex-grow: 1;
-}
+    .intro .wrapper aside {
+      flex-grow: 1;
+    }
 
-.intro .wrapper .photo{
-	background-size: cover;
-	margin-top: 30px;
-}
+    .intro .wrapper .photo {
+      background-size: cover;
+      margin-top: 30px;
+    }
 
-.intro .wrapper .photo img{
-	width: 100%;
-}
+    .intro .wrapper .photo img {
+      width: 100%;
+    }
 
-.intro-social {
-	font-size: 18px; 
-}
+    .intro-social {
+      font-size: 18px;
+    }
 
-.intro-social a{
-	text-decoration: none;
-	color: #000;
-	
-}
+    .intro-social a {
+      text-decoration: none;
+      color: #000;
+    }
 
-.intro-social a:hover{
-	text-decoration: underline;
-}
+    .intro-social a:hover {
+      text-decoration: underline;
+    }
 
     @media only screen and (min-width: 920px) {
+      .intro .wrapper {
+        flex-direction: row;
+      }
 
-
-	.intro .wrapper{
-		flex-direction: row;
-
-	}
-
-	.intro .wrapper .photo{
-		margin-left: 30px;
-		max-width: 45%;
-		margin-top: 0px;
-	}
+      .intro .wrapper .photo {
+        margin-left: 30px;
+        max-width: 45%;
+        margin-top: 0px;
+      }
     }
   `];
 
