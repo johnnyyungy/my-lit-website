b4423a230cc9cdb438c7e3627ea60bce328fd39b
John Yung <johnnyyungy@gmail.com>
2025-01-26 15:19:23 +0000

    fix

--- intro-component.js
+++ intro-component.js
@@ -2,51 +2,60 @@ import { LitElement, html, css } from 'https://cdn.jsdelivr.net/npm/lit@3.2.1/+e
 import { globalStyles } from './global-styles.js';
 
 export class IntroComponent extends LitElement {
-  static styles = [globalStyles, css`
-    .intro {
-      background-color: #fff;
-      min-height: 400px;
-      display: flex;
-    }
-
-    .wrapper {
-      margin: 0 auto;
-      max-width: 1200px;
-      padding: 20px;
-      display: flex;
-      flex-direction: column;
-    }
-
-    .photo img {
-      width: 100%;
-    }
-
-    .intro-social a {
-      text-decoration: none;
-      color: #000;
-    }
-
-    .intro-social a:hover {
-      text-decoration: underline;
-    }
-
-    .photo {
-      margin-top: 30px;
-      max-width: 45%;
-    }
-
-    @media only screen and (min-width: 920px) {
-      .photo {
-        margin-left: 30px;
-        margin-top: 0;
+  static styles = [
+    globalStyles,
+    css`
+      /* Intro */
+      .intro {
+        background-color: #fff;
+        min-height: 400px;
+        display: flex;
       }
 
-      .intro .wrapper{
-		flex-direction: row;
+      .intro .wrapper {
+        flex-direction: column;
+      }
 
-	}
-    }
-  `];
+      .intro .wrapper aside {
+        flex-grow: 1;
+      }
+
+      .intro .wrapper .photo {
+        background-size: cover;
+        margin-top: 30px;
+      }
+
+      .intro .wrapper .photo img {
+        width: 100%;
+      }
+
+      .intro-social {
+        font-size: 18px;
+      }
+
+      .intro-social a {
+        text-decoration: none;
+        color: #000;
+      }
+
+      .intro-social a:hover {
+        text-decoration: underline;
+      }
+
+      /* Media query for wider screens */
+      @media only screen and (min-width: 920px) {
+        .intro .wrapper {
+          flex-direction: row;
+        }
+
+        .intro .wrapper .photo {
+          margin-left: 30px;
+          max-width: 45%;
+          margin-top: 0px;
+        }
+      }
+    `
+  ];
 
   render() {
     return html`
