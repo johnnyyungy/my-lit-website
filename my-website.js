fe739155a7f7ff8fbef9c4fc1ff3cc698496aa02
John Yung <johnnyyungy@gmail.com>
2025-01-26 10:29:26 +0000

    update

--- my-website.js
+++ my-website.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from 'https://cdn.jsdelivr.net/npm/lit@3.2.1/+esm';
 
 class MyWebsite extends LitElement {
   // Rest of your code remains the same
